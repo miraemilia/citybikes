032d3df5c679857c1f2dbd3442c3771286fc9213
Mira Valkama <mira.e.valkama@gmail.com>
2022-08-11 22:16:29 +0300

    JourneyList: journeys listed as table

--- city-bike-front/src/components/JourneyList.tsx
+++ city-bike-front/src/components/JourneyList.tsx
@@ -1,9 +1,35 @@
-import { Container } from "@mui/material"
+import { Container, Paper, Table, TableContainer, TableHead, TableBody, TableRow, TableCell } from "@mui/material"
+import { useAppSelector } from '../hooks'
 
 const JourneyList = () => {
+
+    const journeys = useAppSelector(state => state.journeys.value)
+
     return (
         <Container maxWidth="md">
-            <h3>all journeys...</h3>
+            <h3>Journeys</h3>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>From</TableCell>
+                            <TableCell>To</TableCell>
+                            <TableCell>Distance (m)</TableCell>
+                            <TableCell>Duration (sec)</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {journeys.map((journey) => (
+                            <TableRow key={journey.id} >
+                                <TableCell>{journey.departureStation}</TableCell>
+                                <TableCell>{journey.returnStation}</TableCell>
+                                <TableCell>{journey.distance}</TableCell>
+                                <TableCell>{journey.duration}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </Container>
     )
 }
