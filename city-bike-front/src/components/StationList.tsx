beb7c1a4911910eda6463de0a07f21c1ff5572f6
Mira Valkama <mira.e.valkama@gmail.com>
2022-08-11 23:08:57 +0300

    StationList: stations listed

--- city-bike-front/src/components/StationList.tsx
+++ city-bike-front/src/components/StationList.tsx
@@ -1,9 +1,18 @@
 import { Container } from "@mui/material"
+import { useAppSelector } from "../hooks"
 
 const StationList = () => {
+
+    const stations = useAppSelector(state => state.stations.value)
+
     return (
         <Container maxWidth="md">
-            <h3>all stations...</h3>
+            <h3>Stations</h3>
+            <ul>
+                {stations.map((station) => (
+                    <li key={station.id}>{station.name}</li>
+                ))}
+            </ul>
         </Container>
     )
 }
