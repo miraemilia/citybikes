9cfe359bdc82d3a85909e63f1d6c4c0410b44c25
Mira Valkama <mira.e.valkama@gmail.com>
2022-08-12 22:12:53 +0300

    ViewStation UI changes

--- city-bike-front/src/components/ViewStation.tsx
+++ city-bike-front/src/components/ViewStation.tsx
@@ -33,12 +33,19 @@ const ViewStation = () => {
 
     return (
         <Container maxWidth="md">
-            <h2>{station.name}</h2>
-            <h4>{station.address}</h4>
             <Container maxWidth="sm">
-            <Grid container spacing={8}>
+                <h1></h1>
+            <Grid container columnSpacing={8} rowSpacing={4}>
+                <Grid item xs={12}>
+                    <Card style={{alignContent: 'center', alignItems: 'center'}}>
+                        <CardContent>
+                            <h2>{station.name}</h2>
+                            <h4>{station.address}</h4>
+                        </CardContent>
+                    </Card>
+                </Grid>
                 <Grid item xs={6}>
-                    <Card>
+                    <Card style={{backgroundColor: "lightgrey"}}>
                         <CardContent>
                             <h1>{departureCount}</h1>
                             journeys from the station
@@ -46,14 +53,14 @@ const ViewStation = () => {
                     </Card>
                 </Grid>
                 <Grid item xs={6}>
-                    <Card>
+                    <Card style={{backgroundColor: "lightgrey"}}>
                         <CardContent>
                             <h1>{arrivalCount}</h1>
                             journeys to the station</CardContent>
                     </Card>
                 </Grid>
                 <Grid item xs={6}>
-                    <Card>
+                    <Card style={{backgroundColor: "lightgrey"}}>
                         <CardContent>
                             <h4>TOP 5 return stations</h4>
                             <ol>
@@ -65,7 +72,7 @@ const ViewStation = () => {
                     </Card>
                 </Grid>
                 <Grid item xs={6}>
-                    <Card>
+                    <Card style={{backgroundColor: "lightgrey"}}>
                         <CardContent>
                             <h4>TOP 5 departure stations</h4>
                             <ol>
