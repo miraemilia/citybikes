ead1ab489f08f58eeae7b54131ccf4c81d408f64
Mira Valkama <mira.e.valkama@gmail.com>
2022-08-13 18:17:25 +0300

    Home component updated

--- city-bike-front/src/components/Home.tsx
+++ city-bike-front/src/components/Home.tsx
@@ -3,7 +3,9 @@ import { Container } from "@mui/material"
 const Home = () => {
     return (
         <Container maxWidth="md">
-            <h3>info...</h3>
+            <h3>Info</h3>
+            <p>This app displays data on journeys made with city bikes in the Helsinki Capital area.</p>
+            <p>You can choose a station from the stations list and see data on journeys from and to the station on May 31st 2021. The journeys page lists 100 journeys made on that day.</p>
         </Container>
     )
 }
