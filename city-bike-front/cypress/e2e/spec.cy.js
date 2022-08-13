f3de6a66d3932c66705b3bf834746174c6a4a495
Mira Valkama <mira.e.valkama@gmail.com>
2022-08-13 18:07:51 +0300

    new E2E tests added

--- city-bike-front/cypress/e2e/spec.cy.js
+++ city-bike-front/cypress/e2e/spec.cy.js
@@ -7,4 +7,22 @@ describe('City bike app', () => {
   it('visit front page', () => {
     cy.contains('Helsinki city bike app')
   })
+
+  it('station found on stations page', () => {
+    cy.contains('Stations').click()
+    cy.contains('Unioninkatu')
+  })
+
+  it('station found', () => {
+    cy.contains('Stations').click()
+    cy.contains('Unioninkatu').click()
+    cy.wait(200)
+    cy.get('html').should('not.contain', 'Station not found')
+    cy.contains('Eteläesplanadi 1')
+  })
+
+  it('journeys shown', () => {
+    cy.contains('Journeys').click()
+    cy.contains('Viiskulma')
+  })
 })
