ecf17cc8155e4ede4111963e2e34fcafd28b6bc8
Mira Valkama <mira.e.valkama@gmail.com>
2023-05-16 13:18:08 +0300

    station filter

--- city-bike-front/src/components/StationList.tsx
+++ city-bike-front/src/components/StationList.tsx
@@ -1,17 +1,27 @@
 import { Container, Link } from "@mui/material"
 import { useAppSelector } from "../hooks"
+import { useState } from "react"
 
 const StationList = () => {
 
     const stations = useAppSelector(state => state.stations.value)
+    const [filterString, setFilterString] = useState('')
 
     return (
         <Container maxWidth="md">
             <h3>Stations listing</h3>
+
+            <label htmlFor="filter">Set filter:</label>
+            <input
+                id="filter"
+                value={filterString}
+                onChange={event => setFilterString(event.target.value)}
+            />
+
             <ul>
-                {stations.map((station) => (
-                    <li key={station.id}>
-                        <Link href={`/stations/${station.id}`}>{station.name}</Link>
+                {stations.filter(station => station.name.includes(filterString)).map((filteredStation) => (
+                    <li key={filteredStation.id}>
+                        <Link href={`/stations/${filteredStation.id}`}>{filteredStation.name}</Link>
                     </li>
                 ))}
             </ul>
