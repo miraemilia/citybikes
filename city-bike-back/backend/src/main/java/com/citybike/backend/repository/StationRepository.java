326ab9d07cdd27bb4b5bb2f9e6548eddfbbb645c
Mira Valkama <mira.e.valkama@gmail.com>
2023-05-16 14:58:44 +0300

    StationRepository caching

--- city-bike-back/backend/src/main/java/com/citybike/backend/repository/StationRepository.java
+++ city-bike-back/backend/src/main/java/com/citybike/backend/repository/StationRepository.java
@@ -1,10 +1,13 @@
 package com.citybike.backend.repository;
 
+import javax.persistence.Cacheable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.citybike.backend.model.Station;
 
+@Cacheable
 @Repository
 public interface StationRepository extends JpaRepository<Station, Integer> {
     Station findById(int id);
